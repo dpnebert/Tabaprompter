3874ccf5f18d151f1dfbd4241a8803f78621fcd2
dpnebert <ilovebjs69>
2015-04-02 14:06:41 -0500

    added paneltools

---
+++ Tabaprompter/Tabaprompter/PanelTools.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tabaprompter
+{
+    public class PanelTools
+    {
+
+
+
+        internal static FlowLayoutPanel createFlowPanel(FlowDirection flowDir, string name)
+        {
+
+            FlowLayoutPanel flp = new FlowLayoutPanel();
+            flp.FlowDirection = flowDir;
+            flp.Dock = DockStyle.Fill;
+            flp.AutoSize = true;
+            flp.Name = name;
+            return flp;
+        }
+        internal static Panel createDefaultPanel(string name)
+        {
+            Panel panel = new Panel();
+            panel.Dock = DockStyle.Fill;
+            panel.Name = name;
+            panel.AutoSize = true;
+            return panel;
+        }
+
+
+        internal static Panel buildLogPanel(Panel panel)
+        {
+            FlowLayoutPanel flp = createFlowPanel(FlowDirection.TopDown, "log");
+            panel.Controls.Add(flp);
+            panel.BorderStyle = BorderStyle.Fixed3D;
+            return panel;
+        }
+        internal static Panel buildScrollPanel(Panel panel)
+        {
+            panel.BorderStyle = BorderStyle.Fixed3D;
+            return panel;
+        }
+        internal static Panel buildMarkPanel(Panel panel)
+        {
+            FlowLayoutPanel flp = createFlowPanel(FlowDirection.TopDown, "mark");
+            panel.Controls.Add(flp);
+            return panel;
+        }
+
+        /*
+        internal static Panel buildSelectorPanel(Panel panel)
+        {
+            FlowLayoutPanel flp = createFlowPanel(FlowDirection.TopDown, "selector");
+            panel.Controls.Add(flp);
+            panel.BorderStyle = BorderStyle.Fixed3D;
+
+            // Build combo box controls
+
+
+
+            return panel;
+        }
+        internal static Panel buildControlPanel(Panel panel)
+        {
+            FlowLayoutPanel flp = createFlowPanel(FlowDirection.TopDown, "control");
+            panel.Controls.Add(flp);
+            return panel;
+        }
+        */
+
+
+
+    }
+}
