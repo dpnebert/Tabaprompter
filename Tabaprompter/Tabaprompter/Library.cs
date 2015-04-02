e9fe7d48a6e4f8e8d9656fb68396cffc7a82dd46
dpnebert <ilovebjs69>
2015-04-02 10:39:52 -0500

    Fixed Library not existing

---
+++ Tabaprompter/Tabaprompter/Library.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tabaprompter
+{
+    public class Library
+    {
+        // ID
+        public int ID { get; set; }
+        public List<Tab> tabs { get; set; }
+    }
+}
